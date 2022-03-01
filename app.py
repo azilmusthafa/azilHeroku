9e9a9f8d67e1c84146dd8dd309da9c0f67f465d2
Adhil Ameen <96493753+azilmusthafa@users.noreply.github.com>
2022-03-01 08:56:19 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+'''import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -24,4 +24,38 @@ app.layout = html.Div([
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True) '''
+
+
+from jupyter_dash import JupyterDash
+import dash
+import dash_html_components as html
+import pandas as pd
+ 
+df = pd.read_csv('https://raw.githubusercontent.com/nethajinirmal13/Training-datasets/main/usa-agricultural-exports-2011.csv')
+ 
+ 
+def generate_table(dataframe, max_rows=50):
+   return html.Table([
+       html.Thead(
+           html.Tr([html.Th(col) for col in dataframe.columns])
+       ),
+       html.Tbody([
+           html.Tr([
+               html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+           ]) for i in range(min(len(dataframe), max_rows))
+       ])
+   ])
+ 
+ 
+
+app = dash.Dash(__name__)
+server = app.server
+
+app.layout = html.Div([
+   html.H4(children='US Agriculture Exports (2011)'),
+   generate_table(df)
+])
+ 
+if __name__ == '__main__':
+   app.run_server(debug=True)
